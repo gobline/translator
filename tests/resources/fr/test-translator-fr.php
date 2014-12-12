8e35f0948ba5b990d07b910d5e00867899c83d21
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2014-12-12 16:40:38 +0100

    Fix tests

---
+++ tests/resources/fr/test-translator-fr.php
@@ -0,0 +1,9 @@
+<?php
+
+return  [
+    'First name' => 'Prénom',
+    'Last name' => 'Nom de famille',
+
+    'User %1 created' => 'Utilisateur %1 créé',
+    'User %1 %2 created' => 'Utilisateur %1 %2 créé',
+];
---
+++ tests/resources/nl/testRecursiveDir1/testRecursiveDir2/testRecursiveDir3/test-translator-nl.php
@@ -0,0 +1,9 @@
+<?php
+
+return  [
+    'First name' => 'Voornaam',
+    'Last name' => 'Achternaam',
+
+    'User %1 created' => 'Gebruiker %1 gemaakt',
+    'User %1 %2 created' => 'Gebruiker %1 %2 gemaakt',
+];
